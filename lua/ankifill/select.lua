79606f02d380e435a5b41c6911bdb6a072a855a2
superb00y <krimodrider@gmail.com>
2024-09-23 23:43:32 +0100

    finally image preview

--- lua/ankifill/select.lua
+++ lua/ankifill/select.lua
@@ -56,7 +56,6 @@ Select.SelectImage = function(opts)
   -- local image_formatting = get_config("image_formatting")
   local search_dir = get_config("image_dir")
   local filetypes = { "png", "jpg", "gif" }
-
   local fd = {
     "fd",
     "--type",
@@ -69,61 +68,61 @@ Select.SelectImage = function(opts)
 
   local ub_pid = 0
   local ub_socket = ""
-  local ueberzug_tmp_dir = ""
-  ueberzug_tmp_dir = os.getenv("TMPDIR") or "/tmp"
-  local uuid = vim.fn.getpid()
-  local ub_pid_file = ueberzug_tmp_dir .. "/." .. uuid
+  local ueberzug_tmp_dir = os.getenv("TMPDIR") or "/tmp"
+  local ub_pid_file = ueberzug_tmp_dir .. "/." .. vim.fn.getpid()
   os.execute(string.format("ueberzugpp layer --no-stdin --silent --use-escape-codes --pid-file %s", ub_pid_file))
   local pid_file = io.open(ub_pid_file, "r")
   if pid_file then
     ub_pid = pid_file:read("*n")
     pid_file:close()
     os.remove(ub_pid_file)
-  else
-    print("Error: Failed to read PID file")
-    return
   end
   ub_socket = string.format("%s/ueberzugpp-%s.socket", ueberzug_tmp_dir, ub_pid)
 
-  pickers
-    .new(opts, {
-      prompt_title = "image files",
-      finder = finders.new_oneshot_job(fd, opts),
-      previewer = previewers.new_termopen_previewer({
-        get_command = function(entry, status)
-          local win_info = vim.fn.getwininfo(status.preview_win)[1]
-          local width = win_info.width - 1
-          local height = win_info.height - 1
-          local x = win_info.wincol
-          local y = win_info.winrow
+  local imagepre = previewers.new_buffer_previewer({
+    define_preview = function(_, entry, _)
+      local preview = opts.get_preview_window()
+      local cmd = string.format(
+        "ueberzugpp cmd -s %s -a add -i PREVIEW -x %d -y %d --max-width %d --max-height %d -f %s",
+        ub_socket,
+        preview.col,
+        preview.line,
+        preview.width,
+        preview.height,
+        entry.value
+      )
+      os.execute(cmd)
+    end,
+  })
 
-          local cmd = string.format(
-            "ueberzugpp cmd -s %s -a add -i PREVIEW -x %d -y %d --max-width %d --max-height %d -f %s",
-            ub_socket,
-            x,
-            y,
-            width,
-            height,
-            entry.value
-          )
-          os.execute(cmd)
+  opts.attach_mappings = function(prompt_bufnr, _)
+    actions.select_default:replace(function()
+      local selection = action_state.get_selected_entry()
+      actions.close(prompt_bufnr)
+      os.execute(string.format("ueberzugpp cmd -s %s -a exit", ub_socket))
+      print("Selected image: " .. selection.value)
+    end)
+    return true
+  end
 
-          local exit = string.format("ueberzugpp cmd -s %s -a remove -i PREVIEW", ub_socket)
-          os.execute(exit)
-          -- return cmd
-        end,
-      }),
-      attach_mappings = function(_, map)
-        actions.select_default:replace(function(prompt_bufnr)
-          local selection = action_state.get_selected_entry()
-          actions.close(prompt_bufnr)
-          os.execute(string.format("ueberzugpp cmd -s %s -a exit", ub_socket))
-          print("Selected image: " .. selection.value)
-        end)
-        return true
-      end,
-    })
-    :find()
+  opts.path_display = { "shorten" }
+
+  local popup_opts = {}
+  opts.get_preview_window = function()
+    return popup_opts.preview
+  end
+  local picker = pickers.new(opts, {
+    prompt_title = "image files",
+    finder = finders.new_oneshot_job(fd, opts),
+    previewer = imagepre,
+  })
+
+  local line_count = vim.o.lines - vim.o.cmdheight
+  if vim.o.laststatus ~= 0 then
+    line_count = line_count - 1
+  end
+  popup_opts = picker:get_window_options(vim.o.columns, line_count)
+  picker:find()
 end
 
 return Select
